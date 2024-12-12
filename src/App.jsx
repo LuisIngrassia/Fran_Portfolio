c602950eec29ed7625b1be797cd05124b5de8575
LuisIngrassia <ingrassialuispedro@gmail.com>
2024-12-12 16:41:37 -0300

    Analytics vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import SectionsView from "./pages/sections/SectionsView";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <Router>
+      <Analytics />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sections" element={<SectionsView />} />
