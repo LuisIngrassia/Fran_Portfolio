fa848fe3c0059e74d7107bfb9c10a9b4e133c7d3
LuisIngrassia <ingrassialuispedro@gmail.com>
2024-12-13 09:12:33 -0300

    navBar ajustments

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Menu } from "lucide-react";
+import { Menu, X } from "lucide-react";
 import { HashLink } from "react-router-hash-link";
 
 const NavBar = () => {
@@ -42,57 +42,78 @@ const NavBar = () => {
         scroll ? "bg-black" : "bg-transparent"
       }`}
     >
-      <h1 className="select-none text-2xl font-bold text-[#e6d227]">
-        Franco Cuatto
-      </h1>
+      <HashLink to="/#home" scroll={scrollTo}>
+        <h1 className="select-none text-2xl font-bold text-[#e6d227]">
+          Franco Cuatto
+        </h1>
+      </HashLink>
       <nav>
         <div className="md:hidden">
           <button onClick={toggleMenu} className="text-white">
             <Menu />
           </button>
         </div>
-        <ul
-          className={`absolute left-0 top-16 w-full select-none space-y-2 text-end md:static md:flex md:flex-row md:space-x-4 md:space-y-0 ${
-            isOpen ? "block" : "hidden"
-          } md:block`}
+        {/* Backdrop */}
+        {isOpen && (
+          <div
+            className="fixed inset-0 bg-black bg-opacity-50 md:hidden"
+            onClick={toggleMenu}
+          ></div>
+        )}
+        {/* Drawer Menu */}
+        <div
+          className={`fixed right-0 top-0 h-full w-64 transform bg-black p-4 transition-transform duration-300 ease-in-out md:static md:block md:w-auto md:transform-none md:bg-transparent md:p-0 ${
+            isOpen ? "translate-x-0" : "translate-x-full"
+          }`}
         >
-          <li>
-            <HashLink
-              to="/#home"
-              className="block px-4 py-2 text-white hover:text-[#e6d227]"
-              scroll={scrollTo}
-            >
-              Home
-            </HashLink>
-          </li>
-          <li>
-            <HashLink
-              to="/#sections"
-              className="block px-4 py-2 text-white hover:text-[#e6d227]"
-              scroll={scrollTo}
-            >
-              Sections
-            </HashLink>
-          </li>
-          <li>
-            <HashLink
-              to="/#about"
-              className="block px-4 py-2 text-white hover:text-[#e6d227]"
-              scroll={scrollTo}
-            >
-              About
-            </HashLink>
-          </li>
-          <li>
-            <HashLink
-              to="/#contact"
-              className="block px-4 py-2 text-white hover:text-[#e6d227]"
-              scroll={scrollTo}
-            >
-              Contact
-            </HashLink>
-          </li>
-        </ul>
+          <div className="flex justify-end md:hidden">
+            <button onClick={toggleMenu} className="text-white">
+              <X />
+            </button>
+          </div>
+          <ul className="mt-8 select-none space-y-4 md:mt-0 md:flex md:space-x-4 md:space-y-0">
+            <li>
+              <HashLink
+                to="/#home"
+                className="block px-4 py-2 text-white hover:text-[#e6d227]"
+                scroll={scrollTo}
+                onClick={toggleMenu}
+              >
+                Home
+              </HashLink>
+            </li>
+            <li>
+              <HashLink
+                to="/#sections"
+                className="block px-4 py-2 text-white hover:text-[#e6d227]"
+                scroll={scrollTo}
+                onClick={toggleMenu}
+              >
+                Sections
+              </HashLink>
+            </li>
+            <li>
+              <HashLink
+                to="/#about"
+                className="block px-4 py-2 text-white hover:text-[#e6d227]"
+                scroll={scrollTo}
+                onClick={toggleMenu}
+              >
+                About
+              </HashLink>
+            </li>
+            <li>
+              <HashLink
+                to="/#contact"
+                className="block px-4 py-2 text-white hover:text-[#e6d227]"
+                scroll={scrollTo}
+                onClick={toggleMenu}
+              >
+                Contact
+              </HashLink>
+            </li>
+          </ul>
+        </div>
       </nav>
     </header>
   );
